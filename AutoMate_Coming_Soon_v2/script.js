63269a32ce62cc1751903e34fc0c93edafbd16ce
HunterSL0 <lhudovernik2004@gmail.com>
2025-07-06 16:57:06 +0200

    Add files via upload

---
+++ AutoMate_Coming_Soon_v2/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("email-form").addEventListener("submit", function(e){
+    e.preventDefault();
+    document.getElementById("success-message").style.display = "block";
+});
